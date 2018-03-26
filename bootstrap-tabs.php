53482e4b1f816a47f791c9cedbfe8bc727aba84b
kwintencouton <kwintencouton@users.noreply.github.com>
2018-03-26 15:20:56 +0200

    update 2
    bootstrap tabs

---
+++ bootstrap-tabs.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+    
+    <title> Input database producten</title>
+    <link rel= "shortcut icon" type="image/png" href="beelden/deba.png">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"
+    
+    </head>
+    <body>
+     <nav>
+  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+    <a class="nav-item nav-link active" id="nav-input-tab" data-toggle="tab" href="#nav-input" role="tab" aria-controls="nav-delete" aria-selected="true">input</a>
+    <a class="nav-item nav-link" id="nav-deletetab" data-toggle="tab" href="#nav-delete" role="tab" aria-controls="nav-delete" aria-selected="false">delete</a>
+    <a class="nav-item nav-link" id="nav-update-tab" data-toggle="tab" href="#nav-update" role="tab" aria-controls="nav-update" aria-selected="false">update</a>
+    <a class="nav-item nav-link" id="nav-lijst-tab" data-toggle="tab" href="#nav-lijst" role="tab" aria-controls="nav-lijst" aria-selected="false">lijst</a>
+  </div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
+  <div class="tab-pane fade show active" id="nav-input" role="tabpanel" aria-labelledby="nav-input-tab">...</div>
+  <div class="tab-pane fade" id="nav-delete" role="tabpanel" aria-labelledby="nav-delete-tab">...</div>
+  <div class="tab-pane fade" id="nav-update" role="tabpanel" aria-labelledby="nav-update-tab">...</div>
+   <div class="tab-pane fade" id="nav-lijst" role="tabpanel" aria-labelledby="nav-lijst-tab">...</div>
+</div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        
+        
+    </body>
+    
+</html>
